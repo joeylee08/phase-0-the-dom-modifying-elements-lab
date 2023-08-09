5b14b187aff30a9bc959fed9055f181a3e07e354
Joseph <josephylee72@gmail.com>
2023-08-08 20:35:29 -0700

    completed dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let main = document.querySelector("main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.textContent = "RICKY BOBBY is the champion";
+newHeader.id = "victory";
+document.body.append(newHeader);
